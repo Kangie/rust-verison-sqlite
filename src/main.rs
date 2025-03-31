c30bd758dfd1ac7444fd39f7541dae36a380f0c9
Matt Jolly <kangie@gentoo.org>
2025-03-31 19:11:51 +1000

    Listen to the world!

--- src/main.rs
+++ src/main.rs
@@ -127,7 +127,7 @@ async fn main() -> std::io::Result<()> {
             .service(componentinfoapi)
             .service(Files::new("/static", "./static")) // No need to enable listing
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
